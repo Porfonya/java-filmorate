e2c9b712d593823d9e337f50ad999a966a8296db
Porf Paymulov <p.paymulov@gmail.com>
2023-11-19 15:32:51 +0300

    Sprint10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -11,7 +10,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@Builder
 public class Film {
     private Long id;
 
