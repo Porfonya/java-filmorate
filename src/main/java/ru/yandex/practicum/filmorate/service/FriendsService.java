0f5f5f54ad82f5b63f9103f2908371606f932f87
Porf Paymulov <p.paymulov@gmail.com>
2023-12-17 12:09:55 +0300

    Sprint11

--- src/main/java/ru/yandex/practicum/filmorate/service/FriendsService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FriendsService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Friends;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.dao.FriendsDaoImpl;
--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/FriendsDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/FriendsDaoImpl.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Friends;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.interfaces.FriendsDao;
